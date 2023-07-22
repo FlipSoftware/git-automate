cfe2a8e0e7ba51ac3f2399d4ca469e074f33379e
Breno Rocha <66398400+amindWalker@users.noreply.github.com>
2023-07-21 23:17:24 -0300

    `refactor`: rename functions and menu items
    `fix`: UI now is asynchronous and unblocked by `read` inputs

--- git-automate.sh
+++ git-automate.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 RESET="\e[0m"
 BLACK="\e[30m"
@@ -75,165 +76,201 @@ NEWLINE="\n"
 TAB="\t"
 
 menu_list=(
-    "Show Commits"
-    "Staging Area"
-    "Simple Commit"
-    "Edit Commits"
-    "Semantic Commit"
-    "Delete Commits"
-    "Stash Changes"
-    "Apply Stash"
-    "Show Branches"
-    "Create Branch"
-    "Checkout Branch"
-    "Delete Branch"
-    "Merge Branch"
-    "Tag Commit"
-    "Close Program"
+    "Status"
+    "Staging"
+    "Semantic"
+    "Simple"
+    "Edit"
+    "Delete"
+    "Automate"
+    "Stash"
+    "Branches"
+    "Tag"
+    "Reset"
+    "Revert"
+    "Quit"
 )
 
-function set_terminal_dimensions() {
-    read -r COLUMNS LINES < <(stty size)
+PANELS=("background", "title", "interactive")
+PANELS_LEN=${#PANELS[@]}
+
+get_terminal_size() {
+    read -r ROW COL < <(stty size)
+    PANEL_HEIGHT=$((ROW - (2 * MARGIN)))
+    PANEL_WIDTH=$((COL - (2 * MARGIN)))
+    INDENT=$((PANEL_WIDTH / 100))
+    MARGIN=$((PANEL_WIDTH / 80))
 }
 
-function highlight_item() {
-    printf "$BOLD$YELLOW%s$RESET" "$1"
+move_cursor() {
+    printf "\e[%d;%dH" "$(($1 + 1))" "$(($2 + 1))" # +1 compensates the 0-based index
 }
 
-function reset_terminal() {
-    printf "$RESET"
-    printf "$CURSOR_SHOW"
+display_message() {
+    local message="$1"
+    local message_row=$((ROW / 2))
+    local message_col=$((COL / 2 - (${#message} / 2)))
+    move_cursor "$message_row" "$message_col"
+    echo "$message"
 }
 
-function draw_menu() {
-    local content="$1"
-    local content_width="${#content}"
-    local max_width=$((COLUMNS - 4)) # adjust for box borders
+draw_panel() {
+    local title="$1"
+    local tabulation="${2:-0}"
+    local padding="$((${3:-0} - ${#title} < 0 ? 0 : ${3:-0} - ${#title}))"
+    local panels_num=$((${4:-0} < 0 ? 0 : ${4:-0}))
 
-    if ((content_width > max_width)); then
-        content="${content:0:max_width}"
-        content_width="$max_width"
+    local panel_indent=2
+    local horizontal_line="─$title─"
+    if ((panels_num > 0)); then
+        printf "${panel_indent}╭${horizontal_line//?/─}╮"
+        for ((i = 0; i < panels_num; i++)); do
+            printf "│ ${title} │"
+        done
+        printf "╰${horizontal_line//?/─}╯" # ends with newline
+        ((panel_indent += 2))
     fi
 
+    # Simple bordered text
     local blank="          "
-    local padding=$((17 - content_width))
-    local horizontal_line="─$content${content:0:padding}─"
-
-    printf "╭${horizontal_line//?/─}╮\n"
-    printf "│ ${content} ${blank:0:padding}│\n"
-    printf "╰${horizontal_line//?/─}╯\r"
+    local horizontal_line="─$title${blank:0:padding}─"
+    printf "╭%*s${horizontal_line//?/─}╮\n" $tabulation
+    printf "│%*s ${title}${blank:0:padding} │\n" $tabulation
+    printf "╰%*s${horizontal_line//?/─}╯\r" $tabulation # ends with carriage return
 }
 
-function draw_panel() {
-    local content="$1"
-    local panel_width=$((COLUMNS - 20)) # adjust the width to leave space for the borders and padding
-    local panel_height=$((LINES - 4))   # adjust the height to leave space for the top and bottom borders
+highlight_item() {
+    printf "$BOLD$2%s$RESET" "$1"
+}
+
+render_menu() {
+    local num_items=${#menu_list[@]}
+    move_cursor 3 0
+    draw_panel " COMMITS "
+    for ((i = 0; i < num_items; i++)); do
+        move_cursor $((6 + (i * 2))) ""
+        if [[ $i -eq $SELECTED ]]; then
+            if [[ $i -eq $((num_items - 1)) ]]; then
+                highlight_item "$(draw_panel "➤ ${menu_list[i]}" "" 10)" "$RESET$RED"
+                break
+            fi
+            highlight_item "$(draw_panel "➤ ${menu_list[i]}" "" 10)" "$YELLOW"
+        else
+            printf "$DIM"
+            draw_panel "${menu_list[i]}" 0 10
+            printf "$RESET"
+        fi
+    done
+}
+
+handle_resize() {
+    get_terminal_size
+    if ((COL >= 60 && ROW >= 20)); then
+        draw_interface=true
+    else
+        draw_interface=false
+        display_message "Terminal size is too small to display properly. Please resize the terminal window."
+    fi
+}
+
+background() {
+    local width=$((COLUMNS - 6))
+    local height=$((LINES - 4))
 
     horizontal_line() {
         local char="$1"
-        local times=$2
-        if ((panel_width % 2 == 0)); then
-            printf "%0.s$char" $(seq 1 "$times")
-        else # accounting for odd numbers
-            printf "%0.s$char" $(seq 1 "$((times + 1))")
-        fi
+        printf "%0.s$char" $(seq 1 "$width")
     }
 
     vertical_line() {
         local char="$1"
-        local padding=$((panel_width))
-        printf "\e[19C%s%*s%s\n" "$char" "$padding" "$char"
+        printf "%s%*s%s\n" "$char" "$width" "" "$char"
     }
 
-    printf "╭%s╮\n" "$(horizontal_line "─" "$((panel_width - 4))")"
-    title="GIT AUTOMATE"
-    title_len=0
-    if ((${#title_len} % 2 == 0)); then
-        title_len=${#title}
-    else
-        title_len=$((${#title} - 1))
-    fi
-    # TODO: fix border alignment
-    local left_padding=$((panel_width / 2 + title_len / 2))
-    local right_padding=$((panel_width / 2 - title_len / 2 - 1 - (panel_width % 2)))
-    printf "\e[19C│%*s%*s│\n" "$left_padding" "$title" "$right_padding" ""
+    printf "$DIM$BRIGHT_BLACK"
+    printf "%*s╭%s╮\n" "$((INDENT - 1))" "" "$(horizontal_line "─")"
 
-    local lines_to_print=$((panel_height - 3)) # calculate how many vertical lines to print (excluding top and bottom borders and title lines)
-    for ((i = 0; i < lines_to_print; i++)); do
+    for ((i = 0; i < height; i++)); do
+        printf "%*s" "$((INDENT - 1))" ""
         vertical_line "│"
     done
 
-    printf "\e[19C╰%s╯\n" "$(horizontal_line "─" "$((panel_width - 4))")"
+    printf "%*s╰%s╯\n" "$((INDENT - 1))" "" "$(horizontal_line "─")"
+    printf "$RESET"
 }
 
-function handle_resize() {
-    set_terminal_dimensions
-    redraw_menu
+update_interface() {
+    background
+    render_menu
+
+    draw_interface=false
 }
 
-function redraw_menu() {
-    printf "$CLEAR_SCREEN$CURSOR_HIDE"
-    printf "\e[0;0H"
-    stty -echo
+async_handle_key_press() {
+    IFS= read -rsn1 -t 0.1 key
 
-    local num_items=${#menu_list[@]}
-    for ((i = 0; i < num_items; i++)); do
-        if [[ $i -eq $selected ]]; then
-            highlight_item "$(draw_menu "➤ ${menu_list[i]}")"
-        else
-            printf "$DIM"
-            draw_menu "${menu_list[i]}"
-            printf "$RESET"
-        fi
-    done
-
-    printf "\e[0;20H"
+    case "$key" in
+    "q") # 'q' key pressed, exit the script
+        reset_terminal
+        exit
+        ;;
+    "A") # Up arrow key pressed
+        ((SELECTED = (SELECTED - 1 + num_items) % num_items))
+        draw_interface=true
+        ;;
+    "B") # Down arrow key pressed
+        ((SELECTED = (SELECTED + 1) % num_items))
+        draw_interface=true
+        ;;
+    "C") # Right arrow key pressed
+        ((SELECTED = (SELECTED + 1) % num_items))
+        draw_interface=true
+        ;;
+    "D") # Left arrow key pressed
+        ((SELECTED = (SELECTED - 1 + num_items) % num_items))
+        draw_interface=true
+        ;;
+    $'\0') # Enter key pressed
+        printf "\n\e[2;20H SELECTED: %s\n" "${menu_list[SELECTED]}"
+        ;;
+    esac
 }
 
-function display_menu() {
+reset_terminal() {
+    printf "$CURSOR_SHOW"
+    stty sane
+}
+
+main() {
     local menu=("$@")
     local num_items=${#menu[@]}
 
-    selected=0
-    handle_resize SIGWINCH
-    redraw_menu
+    SELECTED=0
+    draw_interface=true
+
+    # Register the resize signal handler
+    trap handle_resize SIGWINCH
 
     while true; do
-        local key
-        IFS= read -rsn1 key
+        if [ "$draw_interface" = true ]; then
+            clear
+            printf "$CURSOR_HIDE"
+            stty -echo
+            update_interface
+        fi
 
-        case "$key" in
-        "q") # 'q' key pressed, exit the script
-            reset_terminal
-            tput cnorm
-            break
-            ;;
-        "A") # Up arrow key pressed
-            ((selected = (selected - 1 + num_items) % num_items))
-            redraw_menu
-            ;;
-        "B") # Down arrow key pressed
-            ((selected = (selected + 1) % num_items))
-            redraw_menu
-            ;;
-        "C") # Righ arrow key pressed
-            ((selected = (selected + 1) % num_items))
-            redraw_menu
-            ;;
-        "D") # Left arrow key pressed
-            ((selected = (selected - 1 + num_items) % num_items))
-            redraw_menu
-            ;;
-        "") # Enter key pressed
-            local panel_content="This is the panel content"
-            draw_panel "$panel_content"
+        # Set the terminal in non-blocking mode for reading input
+        stty -icanon time 0 min 0
 
-            printf "You selected: %s\n" "${menu[selected]}"
-            ;;
-        esac
+        # Wait for user input with a timeout of 0.1 seconds (auto-select option)
+        # This allows the script to choose the option while unblock resize signals
+        async_handle_key_press
+
+        # Reset the terminal to blocking mode for the trap to work properly
+        stty sane
     done
-
-    exit 0
 }
 
-display_menu "${menu_list[@]}"
+get_terminal_size
+handle_resize
+main "${menu_list[@]}"
