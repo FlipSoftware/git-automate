db2c9d2b3715b118c4b5043883c1a80997450ed9
amindWalker <bhrochamail@gmail.com>
2023-07-25 21:53:42 -0300

    `ci`: new rust ci workflow
    `test`: refactor integration tests

---
+++ packages/git-automate/src/fail/mod.rs
@@ -0,0 +1,2 @@
+pub mod fail;
+pub use fail::*;
--- packages/git-automate/tests/git_commands.rs
+++ packages/git-automate/tests/git_commands.rs
@@ -2,98 +2,48 @@ use miette::IntoDiagnostic;
 use std::process::Command;
 use std::str;
 
-// Helper function to initialize the test repository and commit files
-fn setup_test_repo(workdir: &str) -> miette::Result<()> {
-    miette::set_panic_hook();
-
-    let output = Command::new("git")
-        .args(["init", workdir])
-        .output()
-        .into_diagnostic()?;
-    assert!(output.status.success());
-
-    let curr_dir = std::env::current_dir().into_diagnostic()?;
-    let curr_dir = curr_dir.to_str().unwrap();
-
-    // Set the full path to avoid errors
-    let file_path = format!("{curr_dir}/{workdir}/new_file.txt");
-    std::fs::write(&file_path, "New code").into_diagnostic()?;
-    let output = Command::new("git")
-        .args(["add", &file_path])
-        .current_dir(workdir)
-        .output()
-        .into_diagnostic()?;
-    assert!(output.status.success());
-
-    let output = Command::new("git")
-        .args(["commit", "-m", "Initial commit"])
-        .current_dir(workdir)
-        .output()
-        .into_diagnostic()?;
-    assert!(output.status.success());
-
-    Ok(())
-}
-
-// Helper function to execute Git commands and capture the output with miette
-fn run_git_command(args: &[&str], workdir: &str) -> miette::Result<std::process::Output> {
-    miette::set_panic_hook();
-    Command::new("git")
-        .args(args)
-        .current_dir(workdir)
-        .output()
-        .into_diagnostic()
-}
+use git_automate::*;
 
 #[test]
-fn test_git_status_nothing_to_do() -> miette::Result<()> {
-    let workdir = "test_init";
-    std::process::Command::new("git")
-        .args(["init", workdir])
-        .output()
-        .into_diagnostic()?;
+fn test_git_status() -> miette::Result<()> {
+    std::env::set_current_dir("/var/tmp").into_diagnostic()?;
+    miette::set_panic_hook();
 
-    let output = run_git_command(&["status", "--short", "--branch"], workdir)?;
-    assert!(output.status.success());
-    // Clean up
-    std::fs::remove_dir_all(workdir).into_diagnostic()?;
-
-    let expected_status = "## No commits yet on master";
-    assert_eq!(
-        String::from_utf8_lossy(&output.stdout).trim(),
-        expected_status
-    );
-
-    Ok(())
-}
-
-#[test]
-fn test_git_status_with_changes() -> miette::Result<()> {
-    let workdir = "test_status";
+    let workdir = "test_git_status";
     if std::fs::read_dir(workdir).is_ok() {
         std::fs::remove_dir_all(workdir).into_diagnostic()?;
     }
-    setup_test_repo(workdir)?;
 
-    // Make some changes to files
-    let file_path = format!("{workdir}/new_file.txt");
-    std::fs::write(&file_path, format!("Updated code: {file_path}")).into_diagnostic()?;
+    let init = std::process::Command::new("git")
+        .args(["init", workdir])
+        .output()
+        .into_diagnostic()?;
+    assert!(init.status.success(), "git init failed");
 
-    let output = run_git_command(&["status", "--short", "--branch"], workdir)?;
-    assert!(output.status.success());
+    let file = "new_file.txt";
+    let file_path = format!("{workdir}/{file}");
+    let output = std::fs::write(file_path, "Hello world!");
+    assert!(output.is_ok(), "writing to file failed");
+
+    std::env::set_current_dir(format!("/var/tmp/{workdir}")).into_diagnostic()?;
+
+    let add_file = std::process::Command::new("git")
+        .args(["add", "."])
+        .output()
+        .into_diagnostic()?;
+    assert!(add_file.status.success(), "add file failed");
+
+    let status = git_status(&["status", "--short", "--branch"]);
+    assert!(status.is_ok(), "git status failed");
 
     let branch = std::process::Command::new("git")
         .args(["branch", "--show-current"])
         .output()
         .into_diagnostic()?;
-    let branch = String::from_utf8(branch.stdout).into_diagnostic()?;
-    let expected_status = format!("## {branch} M new_file.txt");
-    assert_eq!(
-        String::from_utf8_lossy(&output.stdout).trim(),
-        expected_status
-    );
+    assert!(branch.status.success(), "git branch faield");
 
-    // Clean up
-    std::fs::remove_dir_all(workdir).into_diagnostic()?;
+    let branch = std::str::from_utf8(&branch.stdout).into_diagnostic()?;
+
+    assert_eq!(status.unwrap().as_str(), format!("## No commits yet on {branch}A  {file}\n"));
     Ok(())
 }
