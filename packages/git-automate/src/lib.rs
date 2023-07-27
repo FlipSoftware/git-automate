a8cac39166cdff0502c1b8f0de80481bc9f3217d
amindWalker <bhrochamail@gmail.com>
2023-07-26 22:26:20 -0300

    `test`: add more tests
    `refactor`: add more refactoring to tests

--- packages/git-automate/src/lib.rs
+++ packages/git-automate/src/lib.rs
@@ -1,14 +1,43 @@
-use std::{process::{Command, Output}};
+use std::process::Output;
 use std::str;
 
 pub mod fail;
 
-pub fn git_command(args: &[&str]) -> Result<Output, std::io::Error> {
+pub fn run_git_command(args: &[&str]) -> Result<Output, std::io::Error> {
     std::process::Command::new("git").args(args).output()
 }
 
+pub fn git_command(args: &[&str], error_message: &str) -> Result<String, fail::IOFail> {
+    let result = run_git_command(args)?;
+    let stdout = String::from_utf8(result.stdout)
+        .map_err(|e| fail::IOFail {
+            message: format_args!("{error_message}\nReturned: {e}").as_str().unwrap(),
+        })?;
+    Ok(stdout)
+}
+
 pub fn git_status(args: &[&str]) -> Result<String, fail::IOFail> {
-    let status = git_command(args);
-    let str = String::from_utf8(status?.stdout);
-    str.map_err(|_| fail::IOFail { message: "git status failed" })
+    git_command(&[&["status"], args].concat(), "git status failed")
+}
+
+pub fn git_log(args: &[&str]) -> Result<String, fail::IOFail> {
+    git_command(&[&["log"], args].concat(), "git log failed")
+}
+
+pub fn git_diff(args: &[&str]) -> Result<String, fail::IOFail> {
+    git_command(&[&["diff"], args].concat(), "git diff failed")
+}
+
+pub fn git_simple_commit(args: &[&str]) -> Result<String, fail::IOFail> {
+    if args.is_empty() {
+        return Err(fail::IOFail { message: "\n\nMessage can not be empty\n\n" })
+    }
+    git_command(&[&["commit", "--message"], args].concat(), "git commit failed")
+}
+
+pub fn git_semantic_commit(args: &[&str], emphasize: bool) -> Result<String, fail::IOFail> {
+    if args.is_empty() {
+        return Err(fail::IOFail { message: "\n\nMessage can not be empty\n\n" })
+    }
+    git_command(&[&["commit", "--message"], args].concat(), "git commit failed")
 }
--- packages/git-automate/tests/git_commands.rs
+++ packages/git-automate/tests/git_commands.rs
@@ -1,49 +1,190 @@
+use std::process::Output;
+
 use miette::IntoDiagnostic;
-use std::process::Command;
-use std::str;
 
 use git_automate::*;
 
-#[test]
-fn test_git_status() -> miette::Result<()> {
+// * Helper function to prepare execution before each test
+fn prepare_test(workdir: &str, file: Option<&str>) -> miette::Result<()> {
+    let file = file.unwrap_or_default();
     std::env::set_current_dir("/var/tmp").into_diagnostic()?;
     miette::set_panic_hook();
 
-    let workdir = "test_git_status";
     if std::fs::read_dir(workdir).is_ok() {
         std::fs::remove_dir_all(workdir).into_diagnostic()?;
     }
 
-    let init = std::process::Command::new("git")
-        .args(["init", workdir])
-        .output()
-        .into_diagnostic()?;
+    let init = test_git_command(&["init", workdir])?;
     assert!(init.status.success(), "git init failed");
 
-    let file = "new_file.txt";
-    let file_path = format!("{workdir}/{file}");
-    let output = std::fs::write(file_path, "Hello world!");
-    assert!(output.is_ok(), "writing to file failed");
+    if !file.is_empty() {
+        let file_path = format!("{workdir}/{file}");
+        let output = std::fs::write(file_path, "Hello world!");
+        assert!(output.is_ok(), "writing to file failed");
 
-    std::env::set_current_dir(format!("/var/tmp/{workdir}")).into_diagnostic()?;
+        std::env::set_current_dir(format!("/var/tmp/{workdir}")).into_diagnostic()?;
 
-    let add_file = std::process::Command::new("git")
-        .args(["add", "."])
+        let stage_files = test_git_command(&["add", "."])?;
+        assert!(stage_files.status.success(), "add to staging failed");
+    }
+
+    Ok(())
+}
+
+// * Helper function to abstract and test git commands with diagnostics
+fn test_git_command(args: &[&str]) -> miette::Result<Output> {
+    std::process::Command::new("git")
+        .args(args)
         .output()
-        .into_diagnostic()?;
-    assert!(add_file.status.success(), "add file failed");
+        .into_diagnostic()
+}
 
-    let status = git_status(&["status", "--short", "--branch"]);
+#[test]
+fn test_git_status_yet_to_commit() -> miette::Result<()> {
+    let workdir = "test_git_status";
+    let file = "new_file.txt";
+    prepare_test(workdir, Some(file))?;
+    // Command from the library to test
+    let status = git_status(&["--short", "--branch"]);
     assert!(status.is_ok(), "git status failed");
 
-    let branch = std::process::Command::new("git")
-        .args(["branch", "--show-current"])
-        .output()
-        .into_diagnostic()?;
-    assert!(branch.status.success(), "git branch faield");
-
+    let branch = test_git_command(&["branch", "--show-current"])?;
+    assert!(branch.status.success(), "git branch failed");
     let branch = std::str::from_utf8(&branch.stdout).into_diagnostic()?;
 
-    assert_eq!(status.unwrap().as_str(), format!("## No commits yet on {branch}A  {file}\n"));
+    let expected_status = format!("## No commits yet on {branch}A  {file}\n");
+    assert_eq!(status.unwrap(), expected_status);
+    Ok(())
+}
+
+#[test]
+fn test_git_status_clean_tree() -> miette::Result<()> {
+    let workdir = "test_git_status";
+    let file = "new_file.txt";
+    prepare_test(workdir, Some(file))?;
+
+    let commit = test_git_command(&["commit", "-m", "Initial commit"])?;
+    assert!(commit.status.success(), "git commit failed");
+    // Command from the library to test
+    let status = git_status(&["--branch"]);
+    assert!(status.is_ok(), "git status failed");
+
+    let branch = test_git_command(&["branch", "--show-current"])?;
+    assert!(branch.status.success(), "git branch failed");
+    let branch = std::str::from_utf8(&branch.stdout).into_diagnostic()?;
+
+    let expected_status = format!("On branch {branch}nothing to commit, working tree clean\n");
+    assert_eq!(status.unwrap(), expected_status);
+    Ok(())
+}
+
+#[test]
+fn test_git_log() -> miette::Result<()> {
+    let workdir = "test_git_log";
+    let file = "new_file.txt";
+    prepare_test(workdir, Some(file))?;
+
+    let commit = test_git_command(&["commit", "-m", "Initial commit"])?;
+    assert!(commit.status.success(), "git commit failed");
+    // Command from the library to test
+    let log = git_log(&["--oneline", "--max-count=1"]);
+    assert!(log.is_ok(), "git log failed");
+
+    assert_eq!(log.unwrap().split_once(' ').unwrap().1, "Initial commit\n");
+    Ok(())
+}
+
+#[test]
+fn test_git_diff() -> miette::Result<()> {
+    let workdir = "test_git_diff";
+    let file = "new_file.txt";
+    prepare_test(workdir, Some(file))?;
+
+    let curr_dir = std::env::current_dir().into_diagnostic()?;
+    let curr_dir = curr_dir.to_str().unwrap();
+    let file_path = format!("{curr_dir}/{file}");
+    let add_changes = std::fs::write(file_path, "\nNew code added");
+    assert!(add_changes.is_ok(), "git commit failed");
+
+    // Command from the library to test
+    let diff = git_diff(&["--shortstat"]);
+    assert!(diff.is_ok(), "git diff failed");
+
+    assert_eq!(
+        diff.unwrap(),
+        " 1 file changed, 2 insertions(+), 1 deletion(-)\n"
+    );
+    Ok(())
+}
+
+#[test]
+fn test_git_simple_commit_fail() -> miette::Result<()> {
+    // Command from the library to test
+    let commit = git_simple_commit(&[]);
+    assert!(commit.is_err(), "git commit should fail");
+
+    assert_eq!(
+        commit.map_err(|e| e.message),
+        Err("\n\nMessage can not be empty\n\n")
+    );
+    Ok(())
+}
+
+#[test]
+fn test_git_simple_commit() -> miette::Result<()> {
+    let workdir = "test_git_simple_commit";
+    let file = "new_file.txt";
+    prepare_test(workdir, Some(file))?;
+
+    // Command from the library to test
+    let commit = git_simple_commit(&["Simple commit"]);
+    assert!(commit.is_ok(), "git commit failed");
+
+    assert!(commit.unwrap().contains("Simple commit"));
+    Ok(())
+}
+
+#[test]
+fn test_git_semantic_commit_fail() -> miette::Result<()> {
+    // Command from the library to test
+    let commit = git_semantic_commit(&[], false);
+    assert!(commit.is_err(), "git commit should fail");
+
+    assert_eq!(
+        commit.map_err(|e| e.message),
+        Err("\n\nMessage can not be empty\n\n")
+    );
+    Ok(())
+}
+
+#[test]
+fn test_git_semantic_commit() -> miette::Result<()> {
+    let workdir = "test_git_semantic_commit";
+    let file = "new_file.txt";
+    prepare_test(workdir, Some(file))?;
+
+    // Command from the library to test
+    let commit = git_semantic_commit(&["fix (new_file): add fix for new_file"], false);
+    assert!(commit.is_ok(), "git commit failed");
+
+    assert!(commit
+        .unwrap()
+        .contains("fix (new_file): add fix for new_file"));
+    Ok(())
+}
+
+#[test]
+fn test_git_semanti_commit_emphasized() -> miette::Result<()> {
+    let workdir = "test_git_semantic_commit";
+    let file = "new_file.txt";
+    prepare_test(workdir, Some(file))?;
+
+    // Command from the library to test
+    let commit = git_semantic_commit(&["`fix` (`new_file`): add fix for new_file"], true);
+    assert!(commit.is_ok(), "git commit failed");
+
+    assert!(commit
+        .unwrap()
+        .contains("`fix` (`new_file`): add fix for new_file"));
     Ok(())
 }
